49acb9a327dc1a80910220349eaf50257c0d8be6
paperplane110 <1374736640@qq.com>
2025-08-28 16:43:46 +0800

    2:39:52 setup hono and add a get api

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,36 +1,24 @@
-import { z } from "zod";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { zValidator } from '@hono/zod-validator'
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth"
+import accounts from "./accounts"
+import { HTTPException } from "hono/http-exception";
 
 export const runtime = "edge";
 
-const app = new Hono().basePath("/api")
+const app = new Hono()
+  .basePath("/api")
 
-app.use("*", clerkMiddleware())
-app.get("/hello", (c) => {
-        const auth = getAuth(c)
-        if (!auth?.userId) {
-            return c.json({
-                message: "You need to login"
-            })
-        }
-        return c.json({
-            message: `Hello! ${auth.userId}` 
-        })
-    })
-    .get(
-        "/hello/:test", 
-        zValidator("param", z.object({
-            test: z.string(),
-        })),
-        (c) => {
-        return c.json({
-            message: "hello",
-            test: c.req.valid("param").test,
-        })
-    })
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
+  }
+
+  return c.json({ error: "Internal error" }, 500)
+})
+
+const routes = app.route("/accounts", accounts)
 
 export const GET = handle(app)
 export const POST = handle(app)
+
+export type AppType = typeof routes
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import {
   ClerkProvider
 } from "@clerk/nextjs"
+import { QueryProvider } from "@/providers/query-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -34,7 +35,9 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {children}
+          <QueryProvider>
+            {children}
+          </QueryProvider>
         </body>
       </html>
     </ClerkProvider>
