0a02c43bf3a26549139bbe9a6335ecc0a3690a02
paperplane110 <1374736640@qq.com>
2025-09-05 18:13:52 +0800

    6:10:38 transaction api

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import accounts from "./accounts"
 import categories from "./categories"
+import transactions from "./transactions"
 
 export const runtime = "edge";
 
@@ -11,6 +12,7 @@ const app = new Hono()
 const routes = app
   .route("/accounts", accounts)
   .route("/categories", categories)
+  .route("/transactions", transactions)
 
 export const GET = handle(app)
 export const POST = handle(app)
--- db/schema.ts
+++ db/schema.ts
@@ -1,20 +1,59 @@
-import { createInsertSchema } from "drizzle-zod"
-import { pgTable, text } from "drizzle-orm/pg-core";
+import { z } from "zod";
+import { createInsertSchema } from "drizzle-zod";
+import { relations } from "drizzle-orm";
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
 export const accounts = pgTable("accounts", {
-    id: text().primaryKey(),
-    plaidId: text("plaid_id"),
-    name: text().notNull(),
-    userId: text("user_id").notNull(),
+  id: text().primaryKey(),
+  plaidId: text("plaid_id"),
+  name: text().notNull(),
+  userId: text("user_id").notNull(),
 });
+
+export const accountsRelations = relations(accounts, ({ many }) => ({
+  transactions: many(transactions)
+}));
 
 export const insertAccountSchema = createInsertSchema(accounts);
 
 export const categories = pgTable("categories", {
-    id: text().primaryKey(),
-    plaidId: text("plaid_id"),
-    name: text().notNull(),
-    userId: text("user_id").notNull(),
+  id: text().primaryKey(),
+  plaidId: text("plaid_id"),
+  name: text().notNull(),
+  userId: text("user_id").notNull(),
 });
 
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  transactions: many(transactions),
+}));
+
 export const insertCategorySchema = createInsertSchema(categories);
+
+export const transactions = pgTable("transactions", {
+  id: text().primaryKey(),
+  amount: integer("amount").notNull(),
+  payee: text("payee").notNull(),
+  notes: text("notes"),
+  date: timestamp("date", { mode: "date" }).notNull(),
+  accountId: text("account_id").references(() => accounts.id, {
+    onDelete: "cascade", // when the account has been deleted, delete all transactions
+  }).notNull(),
+  categoryId: text("category_id").references(() => categories.id, {
+    onDelete: "set null", // when the category has been deleted, set the category_id to null
+  }),
+});
+
+export const transactionsRelations = relations(transactions, ({ one }) => ({
+  accounts: one(accounts, {
+    fields: [transactions.accountId],
+    references: [accounts.id],
+  }),
+  categories: one(categories, {
+    fields: [transactions.categoryId],
+    references: [categories.id],
+  }),
+}));
+
+export const insertTransactionSchema = createInsertSchema(transactions, {
+  date: z.coerce.date(),
+});
