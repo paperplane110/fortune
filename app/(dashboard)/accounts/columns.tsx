1eec526273cb1f60caa0b3e416dbf9270d368f74
paperplane110 <1374736640@qq.com>
2025-09-05 15:05:45 +0800

    5:03:44 done account edit and delete

--- app/(dashboard)/accounts/columns.tsx
+++ app/(dashboard)/accounts/columns.tsx
@@ -6,6 +6,7 @@ import { ColumnDef } from "@tanstack/react-table"
 
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
+import { Actions } from "./actions"
 
 export type ResponseType = InferResponseType<typeof client.api.accounts.$get, 200>["data"][0];
 
@@ -45,5 +46,14 @@ export const columns: ColumnDef<ResponseType>[] = [
             </Button>
         )
     },
+  },
+  {
+    accessorKey: "actions",
+    header: "",
+    cell: ({ row }) => {
+      return (
+        <Actions id={row.original.id} />
+      )
+    }
   },
 ]
---
+++ features/accounts/components/edit-account-sheet.tsx
@@ -0,0 +1,95 @@
+import z from "zod";
+
+import { useGetAccount } from "@/features/accounts/api/use-get-account";
+import { useEditAccount } from "@/features/accounts/api/use-edit-account";
+import { useDeleteAccount } from "@/features/accounts/api/use-delete-account";
+import { useOpenAccount } from "@/features/accounts/hooks/use-open-account";
+import { useConfirm } from "@/hooks/use-confirm"
+import { AccountForm } from "@/features/accounts/components/account-form";
+
+import { insertAccountSchema } from "@/db/schema";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet"
+import { Loader2 } from "lucide-react";
+
+// 定义表单的 schema
+// 这里的 schema 是 ZodObject 对象的一个实例，而非类型
+const formSchema = insertAccountSchema.pick({ name: true });
+
+// 定义表单的值类型
+type FormValues = z.input<typeof formSchema>;
+
+export const EditAccountSheet = () => {
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "You are about to delete this transaction."
+  )
+  const { id, isOpen, onClose } = useOpenAccount()
+  const accountQuery = useGetAccount(id);
+  const editMutation = useEditAccount(id);
+  const deleteMutation = useDeleteAccount(id)
+  const isPending =
+    editMutation.isPending ||
+    deleteMutation.isPending
+
+  const isLoading = accountQuery.isLoading;
+
+  const onSubmit = (values: FormValues) => {
+    editMutation.mutate(values, {
+      onSuccess: () => { onClose(); },
+    });
+  }
+
+  const onDelete = async () => {
+    const ok = await confirm()
+    if (ok) {
+      deleteMutation.mutate(undefined, {
+        onSuccess: () => {
+          onClose()
+        }
+      })
+    }
+  }
+
+  const defaultValues = accountQuery.data ? {
+    name: accountQuery.data.name,
+  } : {
+    name: "",
+  }
+
+  return (
+    <>
+      <ConfirmDialog />
+      <Sheet open={isOpen} onOpenChange={onClose}>
+        <SheetContent className="space-y-4 bg-white">
+          <SheetHeader>
+            <SheetTitle>
+              Edit Account
+            </SheetTitle>
+            <SheetDescription>
+              Edit account details
+            </SheetDescription>
+          </SheetHeader>
+          {isLoading ? (
+            <div className="absolute inset-0 flex items-center justify-center">
+              <Loader2 className="size-4 text-muted-foreground animate-spin" />
+            </div>
+          ) : (
+            <AccountForm
+              id={id}
+              defaultValue={defaultValues}
+              onSubmit={onSubmit}
+              onDelete={onDelete}
+              disabled={isPending}
+            />
+          )}
+        </SheetContent>
+      </Sheet>
+    </>
+  )
+}
