856e0314f8a2d3d490ec77ad462fe4c625900f26
paperplane110 <1374736640@qq.com>
2025-09-26 14:37:07 +0800

    7:54:12 add transaction filter field

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -59,7 +59,7 @@ const TransactionsPage = () => {
         </CardHeader>
         <CardContent>
           <DataTable
-            filterKey="name"
+            filterKey="payee"
             columns={columns}
             data={transactions}
             onDelete={(rows) => {
